7a2fc0877bd085db03d4336c5bb866fad9b40c63
reza golestany <golestany@email.kntu.ac.ir>
2024-11-11 00:54:34 +0330

    add datasource.java

--- src/main/java/DataSource.java
+++ src/main/java/DataSource.java
@@ -1,2 +1,35 @@
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataSource {
+
+    @Getter
+    private static final Connection connection;
+
+    private DataSource() {
+    }
+
+    static {
+        var jdbc = "jdbc:postgresql://localhost:5432/HW9-Twitter";
+
+        try {
+            connection = DriverManager.getConnection(jdbc, "postgres", "postgres");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // JVM Shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
 }
