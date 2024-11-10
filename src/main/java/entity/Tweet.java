f41f3e643445497185dce45334eb5fe8d912d416
reza golestany <golestany@email.kntu.ac.ir>
2024-11-11 00:23:46 +0330

    add pojo classes

--- src/main/java/entity/Tweet.java
+++ src/main/java/entity/Tweet.java
@@ -1,4 +1,24 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Tweet {
+    private long id;
+    private String text;
+    private Tweet retweetTo;
+    private User writer;
+    private List<User> likedBy;
+    private List<User> dislikedBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime editedAt;
+    private List<Tag> tags;
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,21 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String displayName;
+    private String email;
+    private String bio;
+    private LocalDate created;
 }
